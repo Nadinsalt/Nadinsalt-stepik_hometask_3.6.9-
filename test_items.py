1285dc4e975740e1a8fc3feba478f001fbf01c03
Nadinsalt <68907897+Nadinsalt@users.noreply.github.com>
2022-10-28 15:46:37 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 import time
 
 
-def test_button_Add to basket_exists_on_the_page(browser):
+def test_button_Add_to_basket_exists_on_the_page(browser):
     browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/") 
     button = browser.find_element(By.CLASS_NAME, 'btn-add-to-basket')
     assert button is not None, 'button not found'
